b2cd20ce571a758507cdfac5ad24c2edda148b9a
SKYsuiyuan123 <13011316073@163.com>
2020-04-05 15:02:10 +0800

    add: 添加 ts 的函数类型

---
+++ src/example/function-type.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: sunpeiyuan
+ * @Date: 2020-04-05 14:31:02
+ * @LastEditors: sunpeiyuan
+ * @LastEditTime: 2020-04-05 14:31:58
+ * @FilePath: \study-ts-webpack01\src\example\function-type.ts
+ * @Description: ts 的函数类型
+ */
+console.clear();
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@
  * @Author: sunpeiyuan
  * @Date: 2020-03-31 22:37:27
  * @LastEditors: sunpeiyuan
- * @LastEditTime: 2020-04-05 11:47:54
+ * @LastEditTime: 2020-04-05 14:31:20
  * @FilePath: \study-ts-webpack01\src\index.ts
  * @Description: 項目配置文件
  */
 // import "./example/basic-type";
-import "./example/interface-type";
+// import "./example/interface-type";
+import "./example/function-type";
