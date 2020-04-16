5dfee234bb93c65995c756a31212e565c7d61797
SKYsuiyuan123 <13011316073@163.com>
2020-04-16 22:26:48 +0800

    add 添加编译选项

--- src/config/a.ts
+++ src/config/a.ts
@@ -2,8 +2,11 @@
  * @Author: sunpeiyuan
  * @Date: 2020-04-14 22:21:18
  * @LastEditors: sunpeiyuan
- * @LastEditTime: 2020-04-14 22:21:23
+ * @LastEditTime: 2020-04-15 23:17:31
  * @FilePath: \study-ts-webpack01\src\config\a.ts
  * @Description:
  */
 let a = 3;
+let arr1 = [1, 2].map((ele) => ele * 2);
+let arr2 = [...arr1, ...[8, 9, 10]];
+console.log(arr2);
--- src/config/b.ts
+++ src/config/b.ts
@@ -2,8 +2,9 @@
  * @Author: sunpeiyuan
  * @Date: 2020-04-14 22:23:35
  * @LastEditors: sunpeiyuan
- * @LastEditTime: 2020-04-14 22:23:36
+ * @LastEditTime: 2020-04-14 22:43:20
  * @FilePath: \study-ts-webpack01\src\config\b.ts
  * @Description:
  */
 let bb = 11;
+let b = 2;
